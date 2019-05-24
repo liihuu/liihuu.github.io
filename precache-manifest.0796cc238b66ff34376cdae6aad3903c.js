88c8db5271688ee0f35a2e75e7fa0fe6fc1ed6d7
lihu <hu_li888@foxmail.com>
2019-05-24 15:23:26 +0800

    k线demo

---
+++ precache-manifest.0796cc238b66ff34376cdae6aad3903c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5677bb0702c8c9e375526efb354bcbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e3b509a87bd948d9c7",
+    "url": "/static/css/main.f7d38461.chunk.css"
+  },
+  {
+    "revision": "d4c2a6fb096da6b5a1e3",
+    "url": "/static/js/2.aa27e2b4.chunk.js"
+  },
+  {
+    "revision": "d2e3b509a87bd948d9c7",
+    "url": "/static/js/main.81f805c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
