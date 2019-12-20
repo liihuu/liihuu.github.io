7b82a9065dfd515bef49a331086dd6d67f00762b
lihu <hu_li888@foxmail.com>
2019-12-21 00:56:33 +0800

    修改样式

--- precache-manifest.77dcdcfd6e504852e878d6bb31f67b88.js
+++ precache-manifest.77dcdcfd6e504852e878d6bb31f67b88.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "427c17eaa78bf8de4aa63b3bb52bc371",
+    "revision": "4164c3557db2b061a53b77cb270c2f5f",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.78bc0712.chunk.css"
   },
   {
-    "revision": "cfffff5442aa68fbdf5e",
-    "url": "/static/css/main.8d50d27b.chunk.css"
+    "revision": "1212db91df872fd59533",
+    "url": "/static/css/main.bd3936d1.chunk.css"
   },
   {
     "revision": "06f63d603315acd23513",
     "url": "/static/js/2.db3f46f8.chunk.js"
   },
   {
-    "revision": "cfffff5442aa68fbdf5e",
-    "url": "/static/js/main.d3300b6d.chunk.js"
+    "revision": "1212db91df872fd59533",
+    "url": "/static/js/main.65e1686b.chunk.js"
   },
   {
     "revision": "8d3ca72f897ba8cb5acb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adfc857857238d6dd24ab9c67aed8c03.js"
+  "/precache-manifest.77dcdcfd6e504852e878d6bb31f67b88.js"
 );
 
 self.addEventListener('message', (event) => {
