68941efa57d5b4673aed59c38d3309fc954ee6c4
lihu <hu_li888@foxmail.com>
2020-04-11 01:50:22 +0800

    Updates

---
+++ precache-manifest.8f5509db89b1823ec4bce00121df79cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b936b5ba8a18296664629ac91b8d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a79679505716ee5bf9",
+    "url": "/static/css/main.5e84308c.chunk.css"
+  },
+  {
+    "revision": "765c2618e9c6d9555ea3",
+    "url": "/static/js/2.3933fc0a.chunk.js"
+  },
+  {
+    "revision": "d5a79679505716ee5bf9",
+    "url": "/static/js/main.bc5c4f01.chunk.js"
+  },
+  {
+    "revision": "c996b2d91c7307b8e012",
+    "url": "/static/js/runtime-main.9579822a.js"
+  },
+  {
+    "revision": "90fdc299829c6e77552f7be3fb4ddcbc",
+    "url": "/static/media/bg.90fdc299.jpg"
+  },
+  {
+    "revision": "e3150cd23ca00384ecdf1f799e078cbc",
+    "url": "/static/media/qrcode.e3150cd2.png"
+  }
+]);
